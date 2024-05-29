b3979980f2837f834f1edfb4e375f38ae5d6eeee
TheSparta <8343238+TheSpartaPT@users.noreply.github.com>
2024-05-29 19:20:14 +0100

    Moved Sets creation to the constructor
    so it doesn't recreate them every request

--- src/controllers/FikaClientController.ts
+++ src/controllers/FikaClientController.ts
@@ -9,11 +9,17 @@ import { FikaConfig } from "../utils/FikaConfig";
 
 @injectable()
 export class FikaClientController {
+    protected requiredMods: Set<string>;
+    protected allowedMods: Set<string>;
+
     constructor(
         @inject("FikaClientModHashesHelper") protected fikaClientModHashesHelper: FikaClientModHashesHelper,
         @inject("FikaConfig") protected fikaConfig: FikaConfig,
     ) {
-        // empty
+        const config = this.fikaConfig.getConfig();
+
+        this.requiredMods = new Set([...config.client.mods.required, "com.fika.core", "com.SPT.custom", "com.SPT.singleplayer", "com.SPT.core", "com.SPT.debugging"]);
+        this.allowedMods = new Set([...this.requiredMods, ...config.client.mods.optional, "com.bepis.bepinex.configurationmanager"]);
     }
 
     /**
@@ -27,7 +33,7 @@ export class FikaClientController {
      * Handle /fika/client/check/mods
      */
     public handleCheckMods(request: IFikaCheckModRequestData): IFikaCheckModResponse {
-        const fikaConfig = this.fikaConfig.getConfig();
+        const config = this.fikaConfig.getConfig();
 
         const mismatchedMods: IFikaCheckModResponse = {
             forbidden: [],
@@ -36,16 +42,12 @@ export class FikaClientController {
         };
 
         // if no configuration was made, allow all mods
-        if (fikaConfig.client.mods.required.length === 0 && fikaConfig.client.mods.optional.length === 0) {
+        if (config.client.mods.required.length === 0 && config.client.mods.optional.length === 0) {
             return mismatchedMods;
         }
 
-        // these mods are always required, no need to have the user add them to the config
-        const requiredMods = new Set([...fikaConfig.client.mods.required, "com.fika.core", "com.SPT.custom", "com.SPT.singleplayer", "com.SPT.core", "com.SPT.debugging"]);
-        const allowedMods = new Set([...requiredMods, ...fikaConfig.client.mods.optional, "com.bepis.bepinex.configurationmanager"]);
-
         //check for missing required mods first
-        for (const pluginId of requiredMods) {
+        for (const pluginId of this.requiredMods) {
             if (!request[pluginId]) {
                 mismatchedMods.missingRequired.push(pluginId);
             }
@@ -58,7 +60,7 @@ export class FikaClientController {
 
         for (const [pluginId, hash] of Object.entries(request)) {
             // check if the mod isn't allowed
-            if (!allowedMods.has(pluginId)) {
+            if (!this.allowedMods.has(pluginId)) {
                 mismatchedMods.forbidden.push(pluginId);
                 continue;
             }
