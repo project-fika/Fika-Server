e6220621ca3a11c2aa7ce3c8e1d5e5ac62d0be18
trippyone <137233897+trippyone@users.noreply.github.com>
2024-06-20 09:07:57 -0400

    Removed unused imports and variables in constructor

--- src/Fika.ts
+++ src/Fika.ts
@@ -1,8 +1,6 @@
 import { DependencyContainer, inject, injectable } from "tsyringe";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
 import { Overrider } from "./overrides/Overrider";
-import { FikaConfig } from "./utils/FikaConfig";
-import { IWebSocketConnectionHandler } from "@spt/servers/ws/IWebSocketConnectionHandler";
 import { FikaNatPunchRelayService } from "./services/FikaNatPunchRelayService";
 
 @injectable()
@@ -11,7 +9,6 @@ export class Fika {
         @inject("DatabaseServer") protected databaseServer: DatabaseServer,
         @inject("Overrider") protected overrider: Overrider,
         @inject("FikaNatPunchRelayService") protected fikaNatPunchRelayService: FikaNatPunchRelayService,
-        @inject("FikaConfig") protected fikaConfigServer: FikaConfig,
     ) {
         // empty
     }
@@ -21,5 +18,6 @@ export class Fika {
     }
 
     public async postSptLoad(container: DependencyContainer): Promise<void> {
+        // unused
     }
 }
