44fded6b42969a3819cb43144e98d30efd4543e7
Archangel <jesse@archangel.wtf>
2024-08-01 13:58:24 +0200

    Inject DatabaseService, ProfileHelper & TimeUtil

--- src/overrides/services/LocationLifecycleService.ts
+++ src/overrides/services/LocationLifecycleService.ts
@@ -5,23 +5,26 @@ import { HttpResponseUtil } from "@spt/utils/HttpResponseUtil";
 
 import { Override } from "../../di/Override";
 import { FikaMatchService } from "../../services/FikaMatchService";
+import { DatabaseService } from "@spt/services/DatabaseService";
+import { ProfileHelper } from "@spt/helpers/ProfileHelper";
 import { LocationLifecycleService } from "@spt/services/LocationLifecycleService";
 import { IStartLocalRaidRequestData } from "@spt/models/eft/match/IStartLocalRaidRequestData";
 import { IStartLocalRaidResponseData } from "@spt/models/eft/match/IStartLocalRaidResponseData";
 import { ILocationBase } from "@spt/models/eft/common/ILocationBase";
 import { BotGenerationCacheService } from "@spt/services/BotGenerationCacheService";
+import { TimeUtil } from "@spt/utils/TimeUtil";
 
 @injectable()
 export class LocationLifecycleServiceOverride extends Override {
-    profileHelper: any;
-    timeUtil: any;
-    databaseService: any;
     constructor(
+        @inject("DatabaseService") protected databaseService: DatabaseService,
+        @inject("ProfileHelper") protected profileHelper: ProfileHelper,
         @inject("LocationController") protected locationController: LocationController,
         @inject("HttpResponseUtil") protected httpResponseUtil: HttpResponseUtil,
         @inject("FikaMatchService") protected fikaMatchService: FikaMatchService,
         @inject("LocationLifecycleService") protected locationLifecycleService: LocationLifecycleService,
         @inject("BotGenerationCacheService") protected botGenerationCacheService: BotGenerationCacheService,
+        @inject("TimeUtil") protected timeUtil: TimeUtil
     ) {
         super();
     }
