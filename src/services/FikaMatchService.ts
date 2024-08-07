b44b6c888d05cdcb1d93043f719717f5954816e8
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-08-07 22:13:36 +0200

    Handle reconnecting on server

--- src/services/FikaMatchService.ts
+++ src/services/FikaMatchService.ts
@@ -204,7 +204,7 @@ export class FikaMatchService {
             time: data.time,
             raidCode: data.raidCode,
             natPunch: false,
-            isDedicated: false
+            isDedicated: false,
         });
 
         this.addTimeoutInterval(data.serverId);
@@ -236,7 +236,7 @@ export class FikaMatchService {
     public endMatch(matchId: string, reason: FikaMatchEndSessionMessage): void {
         this.logger.info(`Coop session ${matchId} has ended: ${reason}`);
 
-        if(this.fikaDedicatedRaidService.requestedSessions.hasOwnProperty(matchId)) {
+        if (this.fikaDedicatedRaidService.requestedSessions.hasOwnProperty(matchId)) {
             delete this.fikaDedicatedRaidService.requestedSessions[matchId];
         }
 
@@ -303,6 +303,24 @@ export class FikaMatchService {
         }
 
         this.matches.get(matchId).players.set(playerId, data);
+    }
+
+    /**
+     * Sets a player to dead
+     * @param matchId
+     * @param playerId
+     * @param data
+     */
+    public setPlayerDead(matchId: string, playerId: string): void {
+        if (!this.matches.has(matchId)) {
+            return;
+        }
+
+        if (!this.matches.get(matchId).players.has(playerId)) {
+            return;
+        }
+
+        this.matches.get(matchId).players.get(playerId).isDead = true;
     }
 
     /**
