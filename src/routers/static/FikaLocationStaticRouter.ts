0a020cb8d0b07096e0ba3d79f4875947a489ca1f
Archangel <jesse@archangel.wtf>
2024-10-15 19:43:19 +0200

    Revert this file

--- src/routers/static/FikaLocationStaticRouter.ts
+++ src/routers/static/FikaLocationStaticRouter.ts
@@ -4,13 +4,12 @@ import { RouteAction, StaticRouter } from "@spt/di/Router";
 import { IGetRaidConfigurationRequestData } from "@spt/models/eft/match/IGetRaidConfigurationRequestData";
 
 import { FikaLocationCallbacks } from "../../callbacks/FikaLocationCallbacks";
-import { IFikaRaidsResponse } from "../../models/fika/routes/location/IFikaRaidsResponse";
 
 @injectable()
 export class FikaLocationStaticRouter extends StaticRouter {
     constructor(@inject("FikaLocationCallbacks") protected fikaLocationCallbacks: FikaLocationCallbacks) {
         super([
-            new RouteAction("/fika/location/raids", async (url: string, info: IGetRaidConfigurationRequestData, sessionID: string, _output: string): Promise<IFikaRaidsResponse> => {
+            new RouteAction("/fika/location/raids", async (url: string, info: IGetRaidConfigurationRequestData, sessionID: string, _output: string): Promise<string> => {
                 return this.fikaLocationCallbacks.handleGetRaids(url, info, sessionID);
             }),
         ]);
