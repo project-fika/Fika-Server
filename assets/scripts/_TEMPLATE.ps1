9a288c6f78994536595d9e98927a1b43e1dd6122
trippyone <daproblematik@gmail.com>
2025-03-22 11:58:12 -0400

    Refactored the code a bit
    Added a WaitTime var
    Reduced default WaitTime to 2 seconds

--- assets/scripts/_TEMPLATE.ps1
+++ assets/scripts/_TEMPLATE.ps1
@@ -1,11 +1,12 @@
-$profileId = "${profileId}"
-$backendUrl = "${backendUrl}"
-$graphicsMode = $false
-$version = "1.0"
+$Version = "1.0"
+$ProfileId = "${profileId}"
+$BackendUrl = "${backendUrl}"
+$WaitTime = 2
 
-Write-Host "Fika Headless Watcher v$version"
-Write-Host "Profile Id: $profileId"
-Write-Host "Backend Url: $backendUrl"
+Write-Host "Fika Headless Watcher v$Version"
+Write-Host
+Write-Host "Profile Id: $ProfileId"
+Write-Host "Backend Url: $BackendUrl"
 Write-Host
 
 if (!(Test-Path "$PSScriptRoot\BepInEx\plugins\Fika.Headless.dll")) {
@@ -14,8 +15,15 @@ if (!(Test-Path "$PSScriptRoot\BepInEx\plugins\Fika.Headless.dll")) {
     exit
 }
 
-for ($i = 0; $i -lt 3; $i++) {
-    if ([Console]::KeyAvailable) {
+$graphicsMode = $false
+
+for ($i = 0; $i -lt $WaitTime; $i++) {
+	$countDown = $WaitTime - $i
+
+    Write-Progress -Activity "Starting headless in $countDown... press G to start the headless in graphics mode." -PercentComplete ((100/$WaitTime)*($i+1))
+    Start-Sleep -Seconds 1
+
+	if ([Console]::KeyAvailable) {
         $key = [Console]::ReadKey($true).Key
 
 	if ($key -in 'G') {
@@ -23,21 +31,16 @@ for ($i = 0; $i -lt 3; $i++) {
             break
         }
     }
-
-    $countDown = 3 - $i
-
-    Write-Progress -Activity "Starting headless in $countDown... press G to start the headless in graphics mode." -PercentComplete ((100/3)*($i+1))
-    Start-Sleep -Seconds 1
 }
 
-Write-Progress -Activity "Complete" -Completed
+Write-Progress -Activity "Starting headless in..." -Completed
 
 if ($graphicsMode) {
     Write-Host "Starting in graphics mode..."
-    Start-Process "$PSScriptRoot\EscapeFromTarkov.exe" -WorkingDirectory "$PSScriptRoot" -ArgumentList "-token=$profileId", "-config={'BackendUrl':'$backendUrl','Version':'live'}", "--enable-console true" -Wait
+    Start-Process "$PSScriptRoot\EscapeFromTarkov.exe" -WorkingDirectory $PSScriptRoot -ArgumentList "-token=$ProfileId", "-config={'BackendUrl':'$BackendUrl','Version':'live'}", "--enable-console true" -Wait
     exit
 }
 
 while ($true) {
-    Start-Process "$PSScriptRoot\EscapeFromTarkov.exe" -WorkingDirectory "$PSScriptRoot" -ArgumentList "-token=$profileId", "-config={'BackendUrl':'$backendUrl','Version':'live'}", "-batchmode", "-nographics", "--enable-console true" -Wait
+    Start-Process "$PSScriptRoot\EscapeFromTarkov.exe" -WorkingDirectory $PSScriptRoot -ArgumentList "-token=$ProfileId", "-config={'BackendUrl':'$BackendUrl','Version':'live'}", "-batchmode", "-nographics", "--enable-console true" -Wait
 }
